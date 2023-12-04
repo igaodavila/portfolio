b9638d198178a9d0ff54b2c976e3ade0aa748c81
Igor Silva Martins <150184182+igaodavila@users.noreply.github.com>
2023-12-04 16:23:51 -0300

    Add files via upload

---
+++ exerciciosjavascript/exercicio001.js
@@ -0,0 +1,21 @@
+var resultado01 = 6 + 10;
+var resultado02 = 45 - 32;
+var resultado03 = 75 / 5;
+var resultado04 = (81 / 9) * 9;
+var resultado05 = 45 - 15;
+var resultado06 = 60 + 96;
+var resultado07 = 25 + 10;
+var resultado08 = 60 / 15;
+var resultado09 = (1145 * 4) + (56 * 3);
+var resultado10 = (32 * 4) / 2;
+
+alert("O resultado da questão 01 é: " + resultado01);
+alert("O resultado da questão 02 é: " + resultado02);
+alert("O resultado da questão 03 é: " + resultado03);
+alert("O resultado da questão 04 é: " + resultado04);
+alert("O resultado da questão 05 é: " + resultado05);
+alert("O resultado da questão 06 é: " + resultado06);
+alert("O resultado da questão 07 é: " + resultado07);
+alert("O resultado da questão 08 é: " + resultado08);
+alert("O resultado da questão 09 é: " + resultado09);
+alert("O resultado da questão 10 é: " + resultado10);
